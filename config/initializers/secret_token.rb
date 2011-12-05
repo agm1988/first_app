ee94e33e208c132ba36c3fe83f47a13d76b90601
agm <agm@agm-Lenovo-G550.(none)>
2011-12-05 18:43:42 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'd6a4d8adb311093cc99461b642991f4e1816d511c854d1402996f3844e0a314eac6659dfc22a8b903fbb121e4425edae5f5e5c7890949dc7d4ae8255df63458d'
